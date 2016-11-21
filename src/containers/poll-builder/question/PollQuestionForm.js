d57c7d2ad0d90a45cc9d7eb90ea8f6ba38dc106c
Timur Catakli <tcatakli@demandbase.com>
2016-11-21 07:11:54 -0800

    Add buttons and styles

--- src/containers/poll-builder/question/PollQuestionForm.js
+++ src/containers/poll-builder/question/PollQuestionForm.js
@@ -2,17 +2,47 @@ import React from 'react'
 import TextField from 'material-ui/TextField'
 import FlatButton from 'material-ui/FlatButton'
 import {cyan500} from 'material-ui/styles/colors'
+import IconButton from 'material-ui/IconButton'
+import ArrowDropRight from 'material-ui/svg-icons/navigation-arrow-drop-right'
+import {fullWhite} from 'material-ui/styles/colors'
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+import ContentAdd from 'material-ui/svg-icons/content/add'
+import ActionHome from 'material-ui/svg-icons/action/home'
+import RaisedButton from 'material-ui/RaisedButton'
+import FontIcon from 'material-ui/FontIcon'
+
+const style = {
+  marginRight: 20
+}
+
+const styles = {
+  circle: {
+    width: 20,
+    height: 20,
+    borderRadius: '50%',
+    backgroundColor: 'red',
+    padding: 10
+  }
+}
 
 class PollQuestionForm extends React.Component {
   render() {
     return (
       <div>
-          <TextField
-            hintText="Hint Text"
-            floatingLabelText="Floating Label Text"
-          />
-          <FlatButton label="Secondary" secondary={true} backgroundColor={cyan500} />
-        </div>
+        <TextField
+          hintText="Hint Text"
+          floatingLabelText="Floating Label Text"
+        />
+        &nbsp;
+        <FlatButton label="Secondary" backgroundColor={cyan500} />
+        <br />
+        <br />
+        What is your favorite pizza topping?
+        <RaisedButton
+      secondary
+      icon={<ArrowDropRight />}
+    />
+      </div>
     )
   }
 }
